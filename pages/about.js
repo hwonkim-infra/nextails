13c19b15d27d0bfd4e046fb0dcc6c5f63e47d9d2
hwonkim-infra <hawonkim99@gmail.com>
2022-05-24 23:56:26 +0900

    1st Nextjs Navbar

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+export default function About() {
+    return (
+      <div className="flex h-full flex-col justify-center items-center">
+        <h1 className="text-4xl mb-5 font-bold">About</h1>
+        <span className="text-7xl">💬</span>
+      </div>
+    );
+  }
+  
